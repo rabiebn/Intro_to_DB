9389c15b82648d82924ab31bdf512dac9bb20711
rabiebn <rabie.bnmr@gmail.com>
2025-08-18 17:19:40 +0100

    1.1 added error handling

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,16 +1,23 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="root",
-)
+try:
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+    )
 
-mycursor = mydb.cursor()
+    mycursor = mydb.cursor()
 
-mycursor.execute(
-    "CREATE DATABASE IF NOT EXISTS alx_book_store"
-)
+    mycursor.execute(
+        "CREATE DATABASE IF NOT EXISTS alx_book_store"
+    )
 
-mycursor.close()
-mydb.close()
+    print("Database 'alx_book_store' created successfully!")
+except mysql.connector.Error as err:
+    print(f"Something went wrong: {err}")
+finally:
+    if mycursor is not None:
+        mycursor.close()
+    if mydb.is_connected():
+        mydb.close()
